1739095647eab55244912dbfab19d64cf725aa0c
Your Name <your.email@example.com>
2026-06-22 14:52:49 +0800

    Add authentication configuration with OWIN middleware

---
+++ ESTAFF/ESTAFF/App_Start/Startup.Auth.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+using ESTAFF.Models.Data;
+
+namespace ESTAFF
+{
+    public partial class Startup
+    {
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                Authenticationtype = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account/Login"),
+                ExpireTimeSpan = TimeSpan.FromHours(8),
+                SlidingExpiration = true
+            });
+        }
+    }
+}
