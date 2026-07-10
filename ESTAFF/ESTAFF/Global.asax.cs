72c5f7cb4587933e5daf9736d8712d8f4acfe6f5
Your Name <your.email@example.com>
2026-07-10 10:06:06 +0800

    update Global.asax for database initialization

--- ESTAFF/ESTAFF/Global.asax.cs
+++ ESTAFF/ESTAFF/Global.asax.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Data.Entity;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using ESTAFF.Migrations;
+using ESTAFF.Models.Data;
 
 namespace ESTAFF
 {
@@ -12,6 +15,8 @@ namespace ESTAFF
     {
         protected void Application_Start()
         {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<ApplicationDbContext, Configuration>());
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
