0514f27a9c1fd0614c5b48b2f51a70416f06db67
Your Name <your.email@example.com>
2026-07-31 09:00:18 +0800

    fix migration issue

--- ESTAFF/ESTAFF/Global.asax.cs
+++ ESTAFF/ESTAFF/Global.asax.cs
@@ -19,7 +19,6 @@ namespace ESTAFF
             Database.SetInitializer(new MigrateDatabaseToLatestVersion<ApplicationDbContext, Configuration>());
 
             AreaRegistration.RegisterAllAreas();
-            GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
